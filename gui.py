6443506bbc13da7dece8ecf8838be4f6055bd051
Gabriel Venturini <dev.venturini@gmail.com>
2023-11-04 18:39:46 -0300

    edit button and list box!

--- gui.py
+++ gui.py
@@ -3,11 +3,16 @@ import PySimpleGUI as psg# 4.60.5
 
 label = psg.Text("Type in a to-do")
 input_box = psg.InputText(tooltip="Enter to-do", key="to-do")
+list_box = psg.Listbox(values=file_editor.get_todos(), key="items", 
+                       enable_events=True, size=[45, 10])
+
 add_button = psg.Button("Add")
 exit_button = psg.Button("Exit")
+edit_button = psg.Button("Edit")
+
 
 window = psg.Window('To Do List',  
-                    layout=[[label], [input_box, add_button], [exit_button]], 
+                    layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]], 
                     font=('Helvetica', 15))
 
 while True:
@@ -18,6 +23,17 @@ while True:
             new_todo = value["to-do"] + "\n"
             todos.append(new_todo.title())
             file_editor.write_todos(todos)
+            window["items"].update(values=get_todos)
+        case 'Edit':
+            todo_to_edit = value["items"][0]
+            new_todo = value["to-do"]
+            get_todos = file_editor.get_todos()
+            get_index = get_todos.index(todo_to_edit)
+            get_todos[get_index] = new_todo.title() + "\n"
+            file_editor.write_todos(get_todos)
+            window["items"].update(values=get_todos)
+        case 'items':
+            window["to-do"].update(value=value["items"][0])
         case 'Exit':
             break
         case psg.WIN_CLOSED:
